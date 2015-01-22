f772a45e6be232720ee263f7a575b972d9b3b108
deesnow <janos.tarjanyi@gmail.com>
2015-01-22 01:55:55 +0100

    New finger exercise

--- L5 PROBLEM 4.py
+++ L5 PROBLEM 4.py
@@ -6,3 +6,27 @@ def gcdIter(a, b):
 
     returns: a positive integer, the greatest common divisor of a & b.
     '''
+    if a > b:
+        z = b
+    else:
+        z = a
+    while z > 1:
+        if b % z == a % z == 0:
+            result = z
+            break
+        else:
+            z = z-1
+    return z
+
+'''
+orginal code
+
+    while a % testValue != 0 or b % testValue != 0:
+        testValue -= 1
+
+    return testValue
+
+'''
+
+
+
