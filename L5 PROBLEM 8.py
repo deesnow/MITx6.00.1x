8d13972cdb9b06d07345a4242bb2a70b427982dd
deesnow <janos.tarjanyi@gmail.com>
2015-01-24 09:51:23 +0100

    New finger exercise

---
+++ L5 PROBLEM 8.py
@@ -0,0 +1,29 @@
+__author__ = 'suni'
+
+def isIn(char, aStr):
+    '''
+    char: a single character
+    aStr: an alphabetized string
+
+    returns: True if char is in aStr; False otherwise
+    '''
+    if aStr == '':
+        return False
+
+    elif len(aStr) == 1:
+        if char == aStr:
+            return True
+        else:
+            return False
+    else:
+        if char == aStr[len(aStr)/2]:
+            return True
+        elif char < aStr[len(aStr)/2]:
+            return isIn(char, aStr[:len(aStr)/2])
+        elif char > aStr[len(aStr)/2]:
+            return isIn(char, aStr[len(aStr)/2+1:])
+
+
+print isIn('k', 'allnqz')
+
+
